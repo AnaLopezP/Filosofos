17d3c7dc172310f311e588d2c26a02a8a1f463dc
Ana López <anamuyamiga@gmail.com>
2023-04-07 18:26:37 +0200

    no funca?

--- filosofos.py
+++ filosofos.py
@@ -7,7 +7,7 @@ class Filosofo(threading.Thread):
         threading.Thread.__init__(self) #Creamos un hilo
         self.tenedor = tenedor
         self.num = num #Número de filósofo
-        self.temp = self.num + 1%5
+        #self.temp = self.num + 1%5
 
     def comer(self):
         print("El filósofo " + str(self.num) + " come.")
@@ -26,3 +26,27 @@ class Filosofo(threading.Thread):
     def liberarIzq(self):
         print("El filósofo " + str(self.num) + " libera el tenedor izquierdo.")
         self.tenedor[self.num].release() #Liberamos el hilo (tenedor), es decir, abrimos el lock.
+
+    def liberarDcho(self):
+        print("El filósofo " + str(self.num) + " libera el tenedor derecho.")
+        self.tenedor[self.num].release()
+
+    def run(self): #función de acción del hilo
+        while True:
+            self.pensar()
+            self.tenedorIzq()
+            self.tenedorDcha()
+            self.comer()
+            self.liberarIzq()
+            self.liberarDcho()
+
+#CODIGO PRINCIPAL
+Nfil = 5
+tenedor = [1, 1, 1, 1, 1]
+
+for i in range(0, 4):
+    tenedor[i] = threading.BoundedSemaphore(1) #Creamos un semáforo para cada tenedor
+
+for i in range(0, 4):
+    Filosofo(i, tenedor).start()
+    time.sleep(0.5)
